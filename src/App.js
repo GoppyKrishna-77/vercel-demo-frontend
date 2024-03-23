03f6ba9ef3054aa1dd514418a60aa2495e09c881
GoppyKrishna-77 <goppykrishna7077@gmail.com>
2024-03-23 12:14:31 +0530

    Backend Integration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+    const [serverMsg, setServerMsg] = useState("");
+
+  useEffect(() => {
+    axios.get("https://vercel-demo-backend.vercel.app/")
+
+      .then(response => {
+        console.log(response.data);
+        setServerMsg(response.data.serverMsg);
+      })
+      .catch(error => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {serverMsg}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
